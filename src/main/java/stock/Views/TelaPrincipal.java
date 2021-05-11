9bb9dc73af64584776e5d73e79e242a80e25cf2b
CaicoFagundes <caicofagundes@gmail.com>
2021-05-11 01:10:42 -0300

    Adicao de icones no design do menu inicial

--- src/main/java/stock/Views/TelaPrincipal.java
+++ src/main/java/stock/Views/TelaPrincipal.java
@@ -29,6 +29,29 @@ public class TelaPrincipal extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jPanel2 = new javax.swing.JPanel();
+        jPanel3 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        cadProduto = new javax.swing.JMenuItem();
+        altProduto = new javax.swing.JMenuItem();
+        delProduto = new javax.swing.JMenuItem();
+        jMenu2 = new javax.swing.JMenu();
+        cadFornecedor = new javax.swing.JMenuItem();
+        altFornecedor = new javax.swing.JMenuItem();
+        delFornecedor = new javax.swing.JMenuItem();
+        jMenu3 = new javax.swing.JMenu();
+        jMenuItem7 = new javax.swing.JMenuItem();
+        jMenuItem8 = new javax.swing.JMenuItem();
+        jMenuItem9 = new javax.swing.JMenuItem();
+        jMenu4 = new javax.swing.JMenu();
+        jMenuItem10 = new javax.swing.JMenuItem();
+        jMenuItem11 = new javax.swing.JMenuItem();
+        jMenuItem12 = new javax.swing.JMenuItem();
+        jMenu5 = new javax.swing.JMenu();
+        jMenuItem13 = new javax.swing.JMenuItem();
+        jMenuItem14 = new javax.swing.JMenuItem();
+        jMenuItem15 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -36,15 +59,38 @@ public class TelaPrincipal extends javax.swing.JFrame {
 
         jPanel2.setBackground(new java.awt.Color(204, 255, 255));
 
+        jPanel3.setBackground(new java.awt.Color(255, 255, 255));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/stock/Views/Logo.png"))); // NOI18N
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addGap(39, 39, 39)
+                .addComponent(jLabel1)
+                .addContainerGap(40, Short.MAX_VALUE))
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addGap(30, 30, 30)
+                .addComponent(jLabel1)
+                .addContainerGap(34, Short.MAX_VALUE))
+        );
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 749, Short.MAX_VALUE)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 266, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 408, Short.MAX_VALUE)
+            .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
@@ -63,6 +109,98 @@ public class TelaPrincipal extends javax.swing.JFrame {
                 .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
+
+        jMenu1.setIcon(new javax.swing.ImageIcon("C:\\Users\\caico\\Desktop\\round_inventory_2_black_24dp.png")); // NOI18N
+        jMenu1.setText("Produtos");
+
+        cadProduto.setIcon(new javax.swing.ImageIcon(getClass().getResource("/stock/Views/round_add_black_24dp.png"))); // NOI18N
+        cadProduto.setText("Cadastro de Produtos");
+        cadProduto.setActionCommand("Cadastro de Produto");
+        jMenu1.add(cadProduto);
+
+        altProduto.setIcon(new javax.swing.ImageIcon(getClass().getResource("/stock/Views/round_mode_edit_black_24dp.png"))); // NOI18N
+        altProduto.setText("Alteração de Produto");
+        altProduto.setActionCommand("");
+        jMenu1.add(altProduto);
+
+        delProduto.setIcon(new javax.swing.ImageIcon(getClass().getResource("/stock/Views/round_delete_black_24dp.png"))); // NOI18N
+        delProduto.setText("Deletar Produto");
+        delProduto.setToolTipText("");
+        delProduto.setActionCommand("");
+        jMenu1.add(delProduto);
+
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/stock/Views/round_work_black_24dp.png"))); // NOI18N
+        jMenu2.setText("Fornecedores");
+
+        cadFornecedor.setIcon(new javax.swing.ImageIcon(getClass().getResource("/stock/Views/round_add_black_24dp.png"))); // NOI18N
+        cadFornecedor.setText("Cadastro Fornecedor");
+        jMenu2.add(cadFornecedor);
+
+        altFornecedor.setIcon(new javax.swing.ImageIcon(getClass().getResource("/stock/Views/round_mode_edit_black_24dp.png"))); // NOI18N
+        altFornecedor.setText("Alteração de fornecedor");
+        altFornecedor.setActionCommand("Alteração de Fornecedor");
+        jMenu2.add(altFornecedor);
+
+        delFornecedor.setIcon(new javax.swing.ImageIcon(getClass().getResource("/stock/Views/round_delete_black_24dp.png"))); // NOI18N
+        delFornecedor.setText("Deletar Fornecedor");
+        jMenu2.add(delFornecedor);
+
+        jMenuBar1.add(jMenu2);
+
+        jMenu3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/stock/Views/round_store_black_24dp.png"))); // NOI18N
+        jMenu3.setText("Unidades");
+
+        jMenuItem7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/stock/Views/round_add_black_24dp.png"))); // NOI18N
+        jMenuItem7.setText("jMenuItem7");
+        jMenu3.add(jMenuItem7);
+
+        jMenuItem8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/stock/Views/round_mode_edit_black_24dp.png"))); // NOI18N
+        jMenuItem8.setText("jMenuItem8");
+        jMenu3.add(jMenuItem8);
+
+        jMenuItem9.setIcon(new javax.swing.ImageIcon(getClass().getResource("/stock/Views/round_delete_black_24dp.png"))); // NOI18N
+        jMenuItem9.setText("jMenuItem9");
+        jMenu3.add(jMenuItem9);
+
+        jMenuBar1.add(jMenu3);
+
+        jMenu4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/stock/Views/round_local_offer_black_24dp.png"))); // NOI18N
+        jMenu4.setText("Vendas");
+
+        jMenuItem10.setIcon(new javax.swing.ImageIcon(getClass().getResource("/stock/Views/round_add_black_24dp.png"))); // NOI18N
+        jMenuItem10.setText("jMenuItem10");
+        jMenu4.add(jMenuItem10);
+
+        jMenuItem11.setIcon(new javax.swing.ImageIcon(getClass().getResource("/stock/Views/round_mode_edit_black_24dp.png"))); // NOI18N
+        jMenuItem11.setText("jMenuItem11");
+        jMenu4.add(jMenuItem11);
+
+        jMenuItem12.setIcon(new javax.swing.ImageIcon(getClass().getResource("/stock/Views/round_delete_black_24dp.png"))); // NOI18N
+        jMenuItem12.setText("jMenuItem12");
+        jMenu4.add(jMenuItem12);
+
+        jMenuBar1.add(jMenu4);
+
+        jMenu5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/stock/Views/round_manage_accounts_black_24dp.png"))); // NOI18N
+        jMenu5.setText("Usuarios");
+
+        jMenuItem13.setIcon(new javax.swing.ImageIcon(getClass().getResource("/stock/Views/round_add_black_24dp.png"))); // NOI18N
+        jMenuItem13.setText("jMenuItem13");
+        jMenu5.add(jMenuItem13);
+
+        jMenuItem14.setIcon(new javax.swing.ImageIcon(getClass().getResource("/stock/Views/round_mode_edit_black_24dp.png"))); // NOI18N
+        jMenuItem14.setText("jMenuItem14");
+        jMenu5.add(jMenuItem14);
+
+        jMenuItem15.setIcon(new javax.swing.ImageIcon(getClass().getResource("/stock/Views/round_delete_black_24dp.png"))); // NOI18N
+        jMenuItem15.setText("jMenuItem15");
+        jMenu5.add(jMenuItem15);
+
+        jMenuBar1.add(jMenu5);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -114,7 +252,30 @@ public class TelaPrincipal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuItem altFornecedor;
+    private javax.swing.JMenuItem altProduto;
+    private javax.swing.JMenuItem cadFornecedor;
+    private javax.swing.JMenuItem cadProduto;
+    private javax.swing.JMenuItem delFornecedor;
+    private javax.swing.JMenuItem delProduto;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenu jMenu3;
+    private javax.swing.JMenu jMenu4;
+    private javax.swing.JMenu jMenu5;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem10;
+    private javax.swing.JMenuItem jMenuItem11;
+    private javax.swing.JMenuItem jMenuItem12;
+    private javax.swing.JMenuItem jMenuItem13;
+    private javax.swing.JMenuItem jMenuItem14;
+    private javax.swing.JMenuItem jMenuItem15;
+    private javax.swing.JMenuItem jMenuItem7;
+    private javax.swing.JMenuItem jMenuItem8;
+    private javax.swing.JMenuItem jMenuItem9;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
     // End of variables declaration//GEN-END:variables
 }
